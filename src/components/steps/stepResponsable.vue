8326350640a812dc86d30aa5c4c25b6ae6586c6b
SAntoineS <hello@sonwen.ch>
2024-09-20 08:23:20 +0200

    Minimal Demo

--- src/components/steps/stepResponsable.vue
+++ src/components/steps/stepResponsable.vue
@@ -1,62 +1,12 @@
 <script setup lang="ts">
-import {Contact, Info, MapPinHouse, Search, User} from "lucide-vue-next";
+import {User} from "lucide-vue-next";
 import {FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue
-} from "@/components/ui/select";
 import {Input} from "@/components/ui/input";
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
-import {Separator} from "@/components/ui/separator";
-import {Button} from "@/components/ui/button";
-import {ref, inject, Ref} from "vue";
+import {inject, Ref} from "vue";
 
 
 // Variables et références
 const uniteLocale = inject<Ref>("uniteLocale");
-
-const dialog = ref(false);
-
-const exampleType = ['Type 1', 'Type 2', 'Type 3']
-const exampleResp = [
-  {
-    legal_id: 'CHE100000012',
-    name: 'Spital User AG',
-  },
-  {
-    legal_id: 'CHE100039845',
-    name: 'Google Sarl',
-  }
-]
-
-function importResp(responsable) {
-  if (responsable) {
-    const {
-      nom, prenom, telephone, email, sexe, personne_id,
-      adresse_plz4, adresse_plznamk, adresse_deinr, adresse_rue_longname
-    } = responsable;
-
-    Object.assign(uniteLocale.value, {
-      responsableNom: nom,
-      responsablePrenom: prenom,
-      ResponsablenumeroTelephone: telephone,
-      responsableEmail: email,
-      responsableSexe: sexe,
-      responsablePersonneId: personne_id,
-      responsableNpa: adresse_plz4,
-      responsableLocalite: adresse_plznamk,
-      responsableNumeroDeRue: adresse_deinr,
-      responsableRue: adresse_rue_longname,
-    });
-
-    dialog.value = false;
-  }
-}
 </script>
 
 <template>
@@ -66,53 +16,6 @@ function importResp(responsable) {
     <User/>
   </h3>
   <div class="flex gap-3 flex-col">
-    <FormField v-model="uniteLocale.responsablePersonneId" v-slot="{ componentField }" name="responsablePersonneId">
-      <FormItem class="w-1/2">
-        <FormLabel>ID Responsable</FormLabel>
-        <FormControl>
-          <div class="flex items-center gap-2">
-            <Input type="text" v-bind="componentField"/>
-            <Button type="submit" size="sm" variant="tonal">
-              <Search class="mr-2 h-4 w-4"/>
-              Rechercher
-            </Button>
-
-            <TooltipProvider>
-              <Tooltip :delay-duration="200">
-                <TooltipTrigger as-child>
-                  <Info class="w-8 h-8"/>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Pour modifier veuillez passer par l'interface de modification</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </div>
-        </FormControl>
-        <FormMessage/>
-      </FormItem>
-    </FormField>
-    <FormField v-model="uniteLocale.responsableSexe" v-slot="{ componentField }" name="responsableSexe">
-      <FormItem class="w-1/2">
-        <FormLabel>Sexe</FormLabel>
-        <FormControl>
-          <Select v-bind="componentField">
-            <SelectTrigger>
-              <SelectValue placeholder=""/>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Types</SelectLabel>
-                <SelectItem v-for="item in exampleType" :value="item">
-                  {{ item }}
-                </SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </FormControl>
-        <FormMessage/>
-      </FormItem>
-    </FormField>
     <div class="flex gap-3">
       <FormField v-model="uniteLocale.responsablePrenom" v-slot="{ componentField }" name="responsablePrenom">
         <FormItem class="w-full">
@@ -126,101 +29,6 @@ function importResp(responsable) {
       <FormField v-model="uniteLocale.responsableNom" v-slot="{ componentField }" name="responsableNom">
         <FormItem class="w-full">
           <FormLabel>Nom*</FormLabel>
-          <FormControl>
-            <Input class="" type="text" v-bind="componentField"/>
-          </FormControl>
-          <FormMessage/>
-        </FormItem>
-      </FormField>
-    </div>
-  </div>
-  <Separator class="my-5" label=""/>
-
-  <!-- Contact -->
-  <h3 class="flex items-center gap-3 scroll-m-20 text-2xl font-semibold tracking-tight">
-    Contact
-    <Contact/>
-  </h3>
-  <div class="flex gap-3">
-    <FormField v-model="uniteLocale.responsableNumeroTelephone" v-slot="{ componentField }" name="responsableNumeroTelephone">
-      <FormItem class="w-full">
-        <FormLabel>Numéro de téléphone*</FormLabel>
-        <FormControl>
-          <Input class="" type="text" v-bind="componentField"/>
-        </FormControl>
-        <FormMessage/>
-      </FormItem>
-    </FormField>
-    <FormField v-model="uniteLocale.responsableEmail" v-slot="{ componentField }" name="responsableEmail">
-      <FormItem class="w-full">
-        <FormLabel>Email*</FormLabel>
-        <FormControl>
-          <Input class="" type="text" v-bind="componentField"/>
-        </FormControl>
-        <FormMessage/>
-      </FormItem>
-    </FormField>
-  </div>
-  <Separator class="my-5"/>
-
-  <!-- Adresse -->
-  <h3 class="flex items-center gap-3 scroll-m-20 text-2xl font-semibold tracking-tight">
-    Adresse
-    <MapPinHouse/>
-  </h3>
-  <div class="flex gap-3 flex-col">
-    <FormField v-model="uniteLocale.responsablePays" v-slot="{ componentField }" name="responsablePays">
-      <FormItem class="w-full">
-        <FormLabel>Pays*</FormLabel>
-        <FormControl>
-          <Select v-bind="componentField">
-            <SelectTrigger>
-              <SelectValue placeholder=""/>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Types</SelectLabel>
-                <SelectItem v-for="item in exampleType" :value="item">
-                  {{ item }}
-                </SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </FormControl>
-        <FormMessage/>
-      </FormItem>
-    </FormField>
-    <div class="flex gap-3">
-      <FormField v-model="uniteLocale.responsableNpa" v-slot="{ componentField }" name="responsableNpa">
-        <FormItem class="w-full">
-          <FormLabel>NPA*</FormLabel>
-          <FormControl>
-            <Input class="" type="text" v-bind="componentField"/>
-          </FormControl>
-          <FormMessage/>
-        </FormItem>
-      </FormField>
-      <FormField v-model="uniteLocale.responsableLocalite" v-slot="{ componentField }" name="responsableLocalite">
-        <FormItem class="w-full">
-          <FormLabel>Localité*</FormLabel>
-          <FormControl>
-            <Input class="" type="text" v-bind="componentField"/>
-          </FormControl>
-          <FormMessage/>
-        </FormItem>
-      </FormField>
-      <FormField v-model="uniteLocale.responsableRue" v-slot="{ componentField }" name="responsableRue">
-        <FormItem class="w-full">
-          <FormLabel>Rue*</FormLabel>
-          <FormControl>
-            <Input class="" type="text" v-bind="componentField"/>
-          </FormControl>
-          <FormMessage/>
-        </FormItem>
-      </FormField>
-      <FormField v-model="uniteLocale.responsableNumeroDeRue" v-slot="{ componentField }" name="responsableNumeroDeRue">
-        <FormItem class="w-full">
-          <FormLabel>Numéro de rue*</FormLabel>
           <FormControl>
             <Input class="" type="text" v-bind="componentField"/>
           </FormControl>
--- src/validators/zod-validators.ts
+++ src/validators/zod-validators.ts
@@ -1,151 +1,21 @@
 import * as z from "zod";
 
-const swissPhonePattern = /^\+41\d{9}$/
-const frenchPhonePattern = /^\+33\d{9}$/
-
-const s1 = z.string({required_error: 'Champs requis.'}).regex(swissPhonePattern, 'Numéro de téléphone non valide. (+41/+33)');
-const s2 = z.string({required_error: 'Champs requis.'}).regex(frenchPhonePattern, 'Numéro de téléphone non valide. (+41/+33)');
-
 export const formSchema = [
-    // Step Responsable
-    z.object({
-        // Responsable
-        responsablePersonneId: z
-            .optional(),
-        responsableSexe: z
-            .optional(),
-        responsablePrenom: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        responsableNom: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-
-        // Contact
-        responsableNumeroTelephone: z
-            .union([s1,s2]),
-        responsableEmail: z
-            .string({
-                required_error: 'Champs requis.',
-            })
-            .email({
-                message: 'Email non valide.'
-            }),
-
-        // Adresse
-        responsablePays: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        responsableNpa: z
-            .string({
-                required_error: 'Champs requis.',
-            })
-            .min(4, 'NPA non valide.')
-            .max(4, 'NPA non valide.'),
-        responsableLocalite: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        responsableRue: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        responsableNumeroDeRue: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-    }),
-
     // Step Inscription
     z.object({
-        // Unite Légale
-        typeInscription: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        ide: z
-            .string({
-                required_error: 'Champs requis.',
-            })
-            .regex(/^CHE\d{9}$/, {
-                message: "Le format doit être 'CHE' suivi de 9 chiffres",
-            }),
         nameUniteLocal: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
+        .string().trim().min(1, 'Champs requis.'),
+
         formeJuridique: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        pays: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        langue: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        descriptionActiEconomiques: z
-            .string()
-            .optional(),
-        noga: z
-            .string()
-            .optional(),
+        .string().trim().min(1, 'Champs requis.'),
+    }),
 
-        // Adresse
-        npa: z
-            .string({
-                required_error: 'Champs requis.',
-            })
-            .min(4, 'NPA non valide.')
-            .max(4, 'NPA non valide.'),
-        localite: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        rue: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        adresseUniteNumero: z
-            .string({
-                required_error: 'Champs requis.',
-            }),
-        uniteCasePostaleNumero: z
-            .string({
-                required_error: 'Champs requis.',
-            })
-            .optional(),
-        uniteCasePostaleNpa: z
-            .string({
-                required_error: 'Champs requis.',
-            })
-            .optional(),
-        uniteCasePostaleLocalite: z
-            .string({
-                required_error: 'Champs requis.',
-            })
-            .optional(),
+    // Step Responsable
+    z.object({       
+        responsablePrenom: z
+        .string().trim().min(1, 'Champs requis.'),
 
-        // Contact
-        numeroTelephone: z
-            .union([s1,s2]),
-        emailEntreprise: z
-            .string({
-                required_error: 'Champs requis.',
-            })
-            .email({
-                message: 'Email non valide.'
-            }),
-
-        // Autre
-        remarque: z
-            .optional(),
-        attachedFile: z
-            .optional()
+        responsableNom: z
+        .string().trim().min(1, 'Champs requis.'),
     }),
 ]
